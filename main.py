4f570179a300d70a6b7e227c5dfb7be3f69c78fd
IPodstolnyy <ipodstolnyy@spb.beeline.ru>
2025-03-08 12:31:20 +0300

    I added funk shorten_link(token, url)

--- main.py
+++ main.py
@@ -3,11 +3,21 @@ import requests
 from decouple import config
 
 
-headers = {"access_token": config('TOKEN'),
-           "user_id":  config('USER_ID'),
-           "v":  config('VERSION')}
+VERSION = '5.199'
+GETSHORTLINK = 'utils.getShortLink'
+YOUR_LINK = input()
 
-response = requests.get('https://api.vk.ru/method/utils.getServerTime?/',
-                        params=headers)
 
-print(response.text)
+def shorten_link(token, url):
+    headers = {"access_token": token,
+               "v": VERSION,
+               "url": url}
+
+    response = requests.get(f'https://api.vk.ru/method/{GETSHORTLINK}/',
+                            params=headers)
+
+    return response.json()['response']['short_url']
+
+
+if __name__ == "__main__":
+    print('Сокращенная ссылка: ', shorten_link(config('TOKEN'), YOUR_LINK))
