864baf64590c7d3436cd2726c545ef649933a243
IPodstolnyy <ipodstolnyy@spb.beeline.ru>
2025-03-09 13:11:35 +0300

    I added func count_clicks

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import requests
 
 from decouple import config
+from urllib.parse import urlparse
 
 
 VERSION = '5.199'
 GETSHORTLINK = 'utils.getShortLink'
+GETLINKSTATS = 'utils.getLinkStats'
+INTERVAL = 'forever'
 YOUR_LINK = input()
 
 
@@ -12,13 +15,37 @@ def shorten_link(token, url):
     headers = {"access_token": token,
                "v": VERSION,
                "url": url}
-    response = requests.get(f'https://api.vk.ru/method/{GETSHORTLINK}/', 
+    response = requests.get(f'https://api.vk.ru/method/{GETSHORTLINK}/',
                             params=headers)
     try:
-        return response.json()['response']['short_url']
+        return response.json()['response']['key']
     except KeyError:
         return "Вы ввели ошибочную ссылку!"
 
 
+def count_clicks(token, link):
+    headers = {"access_token": token,
+               "key": link,
+               "v": VERSION,
+               "interval": INTERVAL}
+
+    response = requests.get(f'https://api.vk.ru/method/{GETLINKSTATS}/',
+                            params=headers)
+
+    try:
+        for quantity in response.json()['response']['stats']:
+            return quantity['views']
+
+    except KeyError:
+        return "Вы ввели ошибочную сокращенную ссылку!"
+
+
+def is_shorten_link(url):
+    if urlparse(url).scheme:
+        return shorten_link(config('TOKEN'), YOUR_LINK)
+    else:
+        return count_clicks(config('TOKEN'), YOUR_LINK)
+
+
 if __name__ == "__main__":
-    print('Сокращенная ссылка: ', shorten_link(config('TOKEN'), YOUR_LINK))
+    print(is_shorten_link(YOUR_LINK))
